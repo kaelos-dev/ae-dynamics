dee6508a4ca4ee6a1f7f83747f6ff5828ae019e5
kaelos-dev <kaelos.dev@gmail.com>
2026-07-09 14:33:24 +0300

    added base blocks class

---
+++ src/main/java/net/kaelos/aedynamics/block/AEDBlocks.java
@@ -0,0 +1,31 @@
+package net.kaelos.aedynamics.block;
+
+import net.kaelos.aedynamics.AED;
+import net.kaelos.aedynamics.item.AEDItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class AEDBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, AED.MOD_ID);
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        AEDItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
